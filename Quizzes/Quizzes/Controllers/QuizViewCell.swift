66c128a2a882ecda3cace046e6f798fb3aea4479
GMosquera1 <genesismosquera@pursuit.org>
2019-02-04 12:45:25 -0500

    additional set up for ViewCell

--- Quizzes/Quizzes/Controllers/QuizViewCell.swift
+++ Quizzes/Quizzes/Controllers/QuizViewCell.swift
@@ -13,4 +13,129 @@ class QuizViewCell: UICollectionViewCell {
     
     
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellow //override init(frame: CGRect) {
+            //super.init(frame: frame)
+         //   backgroundColor = .red
+            commonInit()
+       // }
+    }
+        required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+            commonInit()
+        }
+        
+        private func commonInit() {
+            setUpView()
+        }
+        
+        
+        public lazy var quizText: UILabel = {
+            let label = UILabel()
+            label.text = "Hola"
+            label.textColor = .black
+            label.font = UIFont.systemFont(ofSize: 22, weight: UIFont.Weight.bold)
+            return label
+        }()
+    
+    
+        
+//        public lazy var optionsButton: UIButton = {
+//            let button = UIButton()
+//            //  button.setTitle(". . .", for: .normal)
+//            button.setImage(UIImage.init(named: "plus-icon"), for: .normal)
+//            button.addTarget(self, action: #selector(addToDatabase), for: .touchUpInside)
+//            return button
+//        }()
+//
+//        @objc func addToDatabase(sender: UIButton) {
+//            print("pressed")
+            //        let alertController = UIAlertController(title: "Default Search", message: "Please enter a default zip code e.g 10023 or city name e.g Miami", preferredStyle: .alert)
+            //        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+            //        let submitAction = UIAlertAction(title: "Submit", style: .default)
+            //        alertController.addAction(cancelAction)
+            //        alertController.addAction(submitAction)
+            // present(alertController, animated: true)
+            
+            //        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            //        alert.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (_) in
+            //            print("User defaults was added to your Quiz Collection")
+            //        }))
+            //        alert.addAction(UIAlertAction(title: "Okay", style: .default , handler: { (_) in
+            //            print("User clicked 'Cancel' button")
+            //        }))
+        
+    
+        
+        
+        func setUpView() {
+            addSubview(quizText)
+            //addSubview(optionsButton)
+            quizText.translatesAutoresizingMaskIntoConstraints = false
+            //optionsButton.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+//                quizText.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+                
+                quizText.leadingAnchor.constraint(equalTo: leadingAnchor), quizText.trailingAnchor.constraint(equalTo: trailingAnchor)
+//                optionsButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor)      , optionsButton.rightAnchor.constraint(equalTo: rightAnchor)
+                ])
+        }
+        //commonInit()
+    
+    
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//        commonInit()
+//    }
+    
+//    private func commonInit() {
+//        setUpView()
+//    }
+//
+
+    
+//    public lazy var optionsButton: UIButton = {
+//        let button = UIButton()
+//        //  button.setTitle(". . .", for: .normal)
+//        button.setImage(UIImage.init(named: "plus-icon"), for: .normal)
+//        button.addTarget(self, action: #selector(addToDatabase), for: .touchUpInside)
+//        return button
+//    }()
+    
+//    @objc func addToDatabase(sender: UIButton) {
+//        print("pressed")
+        //        let alertController = UIAlertController(title: "Default Search", message: "Please enter a default zip code e.g 10023 or city name e.g Miami", preferredStyle: .alert)
+        //        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        //        let submitAction = UIAlertAction(title: "Submit", style: .default)
+        //        alertController.addAction(cancelAction)
+        //        alertController.addAction(submitAction)
+        // present(alertController, animated: true)
+        
+        //        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        //        alert.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (_) in
+        //            print("User defaults was added to your Quiz Collection")
+        //        }))
+        //        alert.addAction(UIAlertAction(title: "Okay", style: .default , handler: { (_) in
+        //            print("User clicked 'Cancel' button")
+        //        }))
+ //   }
+    
+
+//    func setUpView() {
+//        addSubview(quizText)
+//        addSubview(optionsButton)
+//        quizText.translatesAutoresizingMaskIntoConstraints = false
+//        optionsButton.translatesAutoresizingMaskIntoConstraints = false
+//
+//        NSLayoutConstraint.activate([
+//            quizText.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor), quizText.leadingAnchor.constraint(equalTo: leadingAnchor), quizText.trailingAnchor.constraint(equalTo: trailingAnchor), quizText.heightAnchor.constraint(equalTo: safeAreaLayoutGuide.heightAnchor, multiplier: 0.50),
+//            optionsButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor)      , optionsButton.rightAnchor.constraint(equalTo: rightAnchor)
+//            ])
+//    }
+    
+    
+    
 }
+
--- Quizzes/Quizzes/Controllers/QuizViewController.swift
+++ Quizzes/Quizzes/Controllers/QuizViewController.swift
@@ -12,26 +12,58 @@ class QuizViewController: UIViewController {
     
     let quizView = QuizVIew()
     
+    var createModel = [CreateModel]()
+    
     var quizInfo = [QuizCollection](){
         didSet {
             DispatchQueue.main.async {
-//self.quizView.quizCollectionView.reloadData()
+                //self.quizView.quizCollectionView.reloadData()
             }
         }
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        quizView.quizCollectionView.reloadData()
+    }
+    
     @IBOutlet weak var quizCV: UICollectionView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-print(DataPersistenceManager.documentsDirectory())
+        self.view.addSubview(quizView)
+        self.quizView.quizCollectionView.dataSource = self
+        self.quizView.quizCollectionView.delegate = self
+        allQuizzes()
+        print(DataPersistenceManager.documentsDirectory())
+    }
+    func allQuizzes() {
+        
+        QuizAPIClient.getQuiz { (appError, quiz) in
+            if let appError = appError {
+                print(appError.errorMessage())
+            } else if let quiz = quiz {
+                self.quizInfo = quiz
+                dump(self.quizInfo)
+            }
+            
+        }
     }
 }
 
-//extension QuizViewController: UICollectionViewDataSource {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 7
-//    }
-//    
-//}
+
+extension QuizViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return CreateModel.getQuiz().count
+        //quizInfo.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "QuizViewCell", for: indexPath) as? QuizViewCell else { return UICollectionViewCell() }
+        let quiz = CreateModel.getQuiz()[indexPath.row]
+        cell.quizText.text = quiz.quizTitle
+        return cell
+    }
+    
+    
+}
+
